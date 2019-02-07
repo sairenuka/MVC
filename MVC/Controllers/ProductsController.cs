db53e617e41b7e300db17d8b1a1ad7c6d4fb9a2f
sai renuka pericharla <renuka.pericharla@valuelabs.com>
2019-02-07 16:41:58 +0530

    Initial commit

---
+++ MVC/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+namespace MVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        ProductsData db = new ProductsData();
+        public ViewResult Details(int id)
+        {
+            Product product = db.ProductsList.Single(x => x.ProductId == id);
+            return View(product);
+        }
+    }
+}
---
+++ MVC/Models/ProductsData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVC.Models
+{
+    class ProductsData
+    {
+        public IEnumerable<Product> ProductsList
+        {
+            get {
+                List<Product> products = new List<Product>() {
+                new Product {ProductId = 1,Name = "oneplus 6T",Price = 40000.00 },
+
+                 new Product {ProductId = 2,Name = "mi max",Price = 15000.00 }
+
+
+                };
+                return products;
+            }
+        }
+    }
+}
